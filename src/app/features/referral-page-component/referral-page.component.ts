38ee699ae2e0ce7a6ef72c75839eedd0d5a85c01
chewdisout <chewdisout@gmail.com>
2025-11-14 16:08:39 +0200

    Styling and home page changes

--- src/app/features/referral-page-component/referral-page.component.ts
+++ src/app/features/referral-page-component/referral-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-referral-page',
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, TranslatePipe, RouterLink],
   templateUrl: './referral-page.component.html',
   styleUrls: ['./referral-page.component.scss'],
 })
